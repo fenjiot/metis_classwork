df9bb828450d1aa8780e8c48af793fb6078c718f
fenjiot <fenjiot@gmail.com>
2014-10-27 16:22:36 -0400

    Create a hand

---
+++ week_07/refactoring_practice/poker/hand.rb
@@ -0,0 +1,15 @@
+class Hand
+  SIZE_OF_HAND = 5
+
+  def initialize(deck)
+    @hand = []
+    @deck = deck
+    puts deck.cards[0][:rank]
+  end
+
+  def deal
+    puts @deck.cards[0][:rank]
+    @hand = @deck.cards.shift(5)
+    puts @hand
+  end
+end
