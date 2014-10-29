cb49627bb28ab8e517e84b04c9ef619b85d37a3b
fenjiot <fenjiot@gmail.com>
2014-10-29 13:55:37 -0400

    Read in data and print out hotel names

---
+++ week_07/refactoring_practice/hotels/hotel_db.rb
@@ -0,0 +1,22 @@
+require "csv"
+
+class Hotel
+  def initialize
+    puts "HELLO"
+    @database = []
+  end
+
+  def get_db
+    CSV.foreach("hotels.csv", headers: true) do |row|
+     @database << row
+    end
+  end
+
+  def print
+    @database.each { |hotel| puts hotel["Hotel"] }
+  end
+end
+
+hotel = Hotel.new
+hotel.get_db
+hotel.print
